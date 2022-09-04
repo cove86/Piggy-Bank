ff8883fedb2204734feaad367cf5010e1af74d5a
Ashley Cove <ash.cove99@gmail.com>
2022-09-04 13:35:16 +0100

    Added Trigger to schema.sql to insert user into bank with default amounts when registering

--- flaskr/schema.sql
+++ flaskr/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user;
 DROP TABLE IF EXISTS bank;
+DROP TRIGGER IF EXISTS trg_bank;
 
 CREATE TABLE user (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,3 +24,39 @@ CREATE TABLE bank (
   One_Pence INTEGER,
   FOREIGN KEY (user_id) REFERENCES user (id)
 );
+
+CREATE TRIGGER trg_bank
+AFTER INSERT ON user
+BEGIN
+  INSERT INTO bank(
+    user_id,
+    Fifty,
+    Twenty,
+    Ten,
+    Five,
+    Two,
+    One,
+    Fifty_Pence,
+    Twenty_Pence,
+    Ten_Pence,
+    Five_Pence,
+    Two_Pence,
+    One_Pence
+  )
+  VALUES
+  (
+    NEW.id,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0,
+    0
+  );
+  END;
