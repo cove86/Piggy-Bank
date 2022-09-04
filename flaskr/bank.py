45afb5025bc9075ee2cfa1af85b7083f65bbdf6e
Ashley Cove <ash.cove99@gmail.com>
2022-09-04 15:01:36 +0100

    Created Balance html, added to bank.py and updated href's on base.html

--- flaskr/bank.py
+++ flaskr/bank.py
@@ -1,3 +1,4 @@
+from re import L
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, url_for
 )
@@ -11,3 +12,14 @@ bp = Blueprint('bank', __name__)
 @bp.route('/')
 def index():
   return render_template('bank/index.html')
+
+@bp.route('/balance')
+@login_required
+def balance():
+  db = get_db()
+  data = db.execute(
+    'SELECT Fifty, Twenty, Ten, Five, Two, One, Fifty_Pence, Twenty_Pence, Ten_Pence, Five_Pence, Two_Pence, One_Pence'
+    ' FROM bank WHERE user_id = ?', str(g.user['id'])
+  ).fetchone()
+  return render_template('bank/balance.html', data=data)
+
