014e445a54cb0716a916d7c9cd33856ef51b44d3
Ashley Cove <ash.cove99@gmail.com>
2022-09-06 07:48:06 +0100

    Added default value to inputs in balance.html, created deposit and withdraw function in helpers.py and added deposit to balance.py

--- pbank/bank.py
+++ pbank/bank.py
@@ -5,7 +5,7 @@ from werkzeug.exceptions import abort
 
 from pbank.auth import login_required
 from pbank.db import get_db
-from pbank.helpers import return_val
+from pbank.helpers import deposit
 
 bp = Blueprint('bank', __name__)
 
@@ -17,17 +17,25 @@ def index():
 @login_required
 def balance():
   db = get_db()
-  data = db.execute(
+  balance = db.execute(
     'SELECT Fifty, Twenty, Ten, Five, Two, One, Fifty_Pence, Twenty_Pence, Ten_Pence, Five_Pence, Two_Pence, One_Pence'
     ' FROM bank WHERE user_id = ?', str(g.user['id'])
   ).fetchone()
 
   if request.method == "POST":
-    
-    if request.form['form-button'] == 'deposit':
-      balance_input = request.form
+    balance_input = request.form
+    if balance_input['form-button'] == 'deposit':
+      deposit(balance_input, str(g.user['id']))
+      # db.execute(
+      #   'UPDATE bank SET Fifty = Fifty + ?, Twenty = Twenty + ?, Ten = Ten + ?, Five = Five + ?, Two = Two + ?, One = One + ?, Fifty_Pence = Fifty_Pence + ?, Twenty_Pence = Twenty_Pence + ?, Ten_Pence = Ten_Pence + ?, Five_Pence = Five_Pence + ?, Two_Pence = Two_Pence + ?, One_Pence = One_Pence + ? '
+      #   'WHERE user_id = ?', (return_val(balance_input['Fifty']), return_val(balance_input['Twenty']), return_val(balance_input['Ten']), return_val(balance_input['Five']),
+      #   return_val(balance_input['Two']),return_val(balance_input['One']), return_val(balance_input['Fifty_Pence']), return_val(balance_input['Twenty_Pence']),
+      #   return_val(balance_input['Ten_Pence']), return_val(balance_input['Five_Pence']),return_val(balance_input['Two_Pence']), return_val(balance_input['One_Pence']), str(g.user['id']))
+      # )
+      # db.commit()
+    elif balance_input['form-button'] == 'withdraw':
       db.execute(
-        'UPDATE bank SET Fifty = Fifty + ?, Twenty = Twenty + ?, Ten = Ten + ?, Five = Five + ?, Two = Two + ?, One = One + ?, Fifty_Pence = Fifty_Pence + ?, Twenty_Pence = Twenty_Pence + ?, Ten_Pence = Ten_Pence + ?, Five_Pence = Five_Pence + ?, Two_Pence = Two_Pence + ?, One_Pence = One_Pence + ? '
+        'UPDATE bank SET Fifty = Fifty - ?, Twenty = Twenty - ?, Ten = Ten - ?, Five = Five - ?, Two = Two - ?, One = One - ?, Fifty_Pence = Fifty_Pence - ?, Twenty_Pence = Twenty_Pence - ?, Ten_Pence = Ten_Pence - ?, Five_Pence = Five_Pence - ?, Two_Pence = Two_Pence - ?, One_Pence = One_Pence - ? '
         'WHERE user_id = ?', (return_val(balance_input['Fifty']), return_val(balance_input['Twenty']), return_val(balance_input['Ten']), return_val(balance_input['Five']),
         return_val(balance_input['Two']),return_val(balance_input['One']), return_val(balance_input['Fifty_Pence']), return_val(balance_input['Twenty_Pence']),
         return_val(balance_input['Ten_Pence']), return_val(balance_input['Five_Pence']),return_val(balance_input['Two_Pence']), return_val(balance_input['One_Pence']), str(g.user['id']))
@@ -35,5 +43,5 @@ def balance():
       db.commit()
     return redirect('/balance')
 
-  return render_template('bank/balance.html', data=data)
+  return render_template('bank/balance.html', balance=balance)
 
--- pbank/helpers.py
+++ pbank/helpers.py
@@ -1,2 +1,24 @@
+import imp
+from webbrowser import get
+from pbank.db import get_db
+
 def return_val(val):
   return val if val != "" else "0"
+
+
+def deposit(new_balance, user_id):
+  db = get_db()
+  db.execute(
+      'UPDATE bank SET Fifty = Fifty + ?, Twenty = Twenty + ?, Ten = Ten + ?, Five = Five + ?, Two = Two + ?, One = One + ?, Fifty_Pence = Fifty_Pence + ?, Twenty_Pence = Twenty_Pence + ?, Ten_Pence = Ten_Pence + ?, Five_Pence = Five_Pence + ?, Two_Pence = Two_Pence + ?, One_Pence = One_Pence + ? '
+      'WHERE user_id = ?', (new_balance['Fifty'], new_balance['Twenty'], new_balance['Ten'], new_balance['Five'],new_balance['Two'],new_balance['One'], new_balance['Fifty_Pence'], new_balance['Twenty_Pence'],new_balance['Ten_Pence'], new_balance['Five_Pence'],new_balance['Two_Pence'], new_balance['One_Pence'], user_id)
+    )
+  db.commit()
+
+
+def withdraw(balance, new_balance, user_id):
+  db = get_db()
+  db.execute(
+    'UPDATE bank SET Fifty = ?, Twenty = ?, Ten = ?, Five = ?, Two = ?, One = ?, '
+    'Fifty_Pence = ?, Twenty_Pence = ?, Ten_Pence = ?, Five_Pence = ?, Two_Pence = ?', 
+    'One_Pence = ? WHERE user_id = ?', 
+  )
